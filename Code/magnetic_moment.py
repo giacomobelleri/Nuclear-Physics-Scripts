414b8373f9d3d2f653496c40ed2843113b3f2d14
giacomobelleri <119069713+giacomobelleri@users.noreply.github.com>
2023-09-21 14:26:38 +0200

    Added new decay computations

--- Code/magnetic_moment.py
+++ Code/magnetic_moment.py
@@ -4,9 +4,20 @@ import matplotlib.pyplot as plt
 import functions.nuclearfunc as nf
 
 # Magnetic moment of nucleus:
-[A, Z] = [197, 79]
-[j, l] = [11*0.5, 5]
+[A, Z] = [43, 20]
+[j, l] = [7*0.5, 5]
 # mag_moment = round(nf.MagneticDipoleMoment(A, Z, j, l, s), 2)
 mag_moment = nf.MagneticDipoleMoment(A, Z, j, l)
 print(f"For a Nucleus with A = {A}, Z = {Z}, j = {j} and l = {l}:\n" +
       f"--Magnetic moment: {mag_moment} mu_N \n")
+
+# Mass of electron
+m_e = 9.11*10**(-31)  # kg
+# Charge of electron
+e = 1.602*10**(-19)  # C
+
+B = 1  # T
+
+# Magnetic resonance frequency
+w = mag_moment*e*B/((2*m_e)*7/2)
+print(w*10**(-14)/(2*np.pi))
--- Code/mass_binding_energy_script.py
+++ Code/mass_binding_energy_script.py
@@ -10,11 +10,17 @@ print(f"For a Nucleus with A = {A} and Z = {Z}:\n"
       f"--Binding energy:  {nf.BindingEnergy(A,Z)} MeV \n" +
       f"--Nuclear mass:    {nf.NuclearMass(A,Z)} MeV/c^2 \n")
 
-# Computes Q-Value of alpha decay
+# Computes Q-Value of alpha decay for nuclei that adhere to SEMF
 [A_i, Z_i] = [212, 84]
 [A_f, Z_f] = [208, 82]
 m_alpha = 4.00260325413 # MeV/c^2
 m_u = 931.49410242 # MeV/c^2
 Q = nf.NuclearMass(A_i, Z_i) - (nf.NuclearMass(A_f, Z_f) + m_alpha*m_u)
 print(f"({A_i}, {Z_i}) --> ({A_f}, {Z_f}) + (4, 2)\n" +
+      f"--Q-Value: {Q} MeV \n")
+
+# Computes Q-Value of symmetric fission decay for nuclei that adhere to the SEMF
+[A_i, Z_i] = [212, 84]
+Q = nf.NuclearMass(A_i, Z_i) - 2*nf.NuclearMass(A_i/2, Z_i/2)
+print(f"({A_i}, {Z_i}) --> ({A_f/2}, {Z_f/2}) + ({A_f/2}, {Z_f/2})\n" +
       f"--Q-Value: {Q} MeV \n")
